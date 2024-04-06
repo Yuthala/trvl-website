02085bb0556177fb4d80dcb08ec0dba634d8bf14
Dina Polousova <dinapolousova@gmail.com>
2024-04-06 19:18:15 +0300

    Hero section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path='/' exact />
-        </Routes>
+          <Routes>
+            <Route path='/' exact element={<Home />}/>
+          </Routes>
       </Router>
     </>
   );

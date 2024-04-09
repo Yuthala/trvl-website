278925109dfbc61afb6615735a47f946d910b2e6
Dina Polousova <dinapolousova@gmail.com>
2024-04-09 08:41:13 +0300

    added items to card.js

--- src/card.js
+++ src/card.js
@@ -1,5 +1,8 @@
 import image9 from './images/img-9.jpg';
 import image2 from './images/img-2.jpg';
+import image3 from './images/img-3.jpg';
+import image4 from './images/img-4.jpg';
+import image8 from './images/img-8.jpg';
 
 
 const cards = [
@@ -9,13 +12,30 @@ const cards = [
         text: 'Explore the hidden waterfall deep inside  the Amazon jungle',
         label: 'Adventure',
         path: '/services'
-    },
-    {
+    },{
         id: 2,
         src: image2,
         text: 'Travel through the Islands of Bali in a Private Cruise',
         label: 'Luxury',
         path: '/services'
+    }, {
+        id: 3,
+        src: image3,
+        text: 'Set Sail in the Atlantic Ocean visiting Uncharted Waters',
+        label: 'Mystery',
+        path: '/services'
+    }, {
+        id: 4,
+        src: image4,
+        text: 'Experience Football on Top of the Himilayan Mountains',
+        label: 'Adventure',
+        path: '/products'
+    }, {
+        id: 5,
+        src: image8,
+        text: 'Ride through the Sahara Desert on a guided camel tour',
+        label: 'Adrenaline',
+        path: '/sign-up'
     }
 ];
 
